3a7a704e842202db6275c3d053400f7601ebae19
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-08 13:01:46 +0600

    cryptography codes

--- 2.cpp
+++ 2.cpp
@@ -6,6 +6,49 @@
 #include <string>
 using namespace std;
 
+char EncryptCharacter(char flag, int shift)
+{
+    if ((flag >= 'A' && flag <= 'Z') || (flag >= 'a' && flag <= 'z'))
+    {
+
+        if (flag >= 'A' && flag <= 'Z')
+        {
+
+            int code = (int)flag + shift;
+
+            code = code - (int)'A';
+
+            if ((code - 26) >= 0)
+            {
+                code = code - 26;
+            }
+
+            code = code + (int)'A';
+
+            flag = (char)code;
+        }
+
+        if (flag >= 'a' && flag <= 'z')
+        {
+            int code = (int)flag + shift;
+
+            code = code - (int)'a';
+
+            if ((code - 26) >= 0)
+            {
+                code = code - 26;
+            }
+
+            code = code + (int)'a';
+
+            flag = (char)code;
+        }
+    }
+
+    return flag;
+}
+
+
 string PlainTextToCipther(string text)
 {
     for(int i = 0; i < text.size(); i = i+3)
@@ -13,53 +56,42 @@ string PlainTextToCipther(string text)
         for(int j = i; j < i+3 ; j++)
         {
             /**
-             * if there is single character left
+             * if there is single character left 5
             */
             if(j+1 >= text.size())
             {
-                if ((text[j] >= 'A' && text[j] <= 'Z') || (text[j] >= 'a' && text[j] <= 'z'))
-                {
+                text[j] = EncryptCharacter(text[j],5);
 
-                    if (text[j] >= 'A' && text[j] <= 'Z')
-                    {
-
-                        int code = (int)text[j] + 5;
-
-                        code = code - (int)'A';
-
-                        if ((code - 26) >= 0)
-                        {
-                            code = code - 26;
-                        }
-
-                        code = code + (int)'A';
-
-                        text[j] = (char)code;
-                    }
-
-                    if (text[j] >= 'a' && text[j] <= 'z')
-                    {
-                        int code = (int)text[j] + 5;
-
-                        code = code - (int)'a';
-
-                        if ((code - 26) >= 0)
-                        {
-                            code = code - 26;
-                        }
-
-                        code = code + (int)'a';
-
-                        text[j] = (char)code;
-                    }
-                }
+                j = j+3;
+                continue;
             }
 
             /**
-             * if there is two character left
+             * if there is two character left 7 11
             */
-           
-            if()
+
+            if(j+2 >= text.size())
+            {
+                text[j] = EncryptCharacter(text[j], 7);
+
+                text[j+1] = EncryptCharacter(text[j+1], 11);
+
+                j = j+3;
+                continue;
+            }
+
+            /**
+             * increasing order 2 11 3
+            */
+
+            if((text[j] <= text[j+1]) && (text[j+1] <= text[j+2]))
+            {
+                text[j] = EncryptCharacter(text[j], 2);
+
+                text[j+1] = EncryptCharacter(text[j+1], 11);
+                
+                text[j+2] = EncryptCharacter(text[j+2], 3);
+            }
 
         }// j loop
     }// i loop
