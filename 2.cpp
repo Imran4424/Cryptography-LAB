fda689e63ef8c150b19d2b065d68cab81b57768a
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-08 12:06:12 +0600

    cryptography codes

--- 2.cpp
+++ 2.cpp
@@ -1,11 +1,40 @@
+/**
+ * 
+*/
+
 #include <iostream>
 #include <string>
 using namespace std;
 
+string PlainTextToCipther(string text)
+{
+    for(int i = 0; i < text.size(); i++)
+    {
+        for(int j=i; j < i+2 ; j++)
+        {
+
+        }
+
+        i = i+2;
+    }
+}
+
+string CiptherToPlainText(string encrypt)
+{
+    for (int i = 0; i < encrypt.size(); i = i+3)
+    {
+        for (int j = i; j < i + 3; j++)
+        {
+
+        }
+    }
+}
+
 main(int argc, char const *argv[])
 {
-    string    
+   string text;
 
+   getline(cin,text); 
 
     return 0;
 }
