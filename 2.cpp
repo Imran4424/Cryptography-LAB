5fc9ef10b80b4e0fc3631d04fbdb1050b83755b5
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-08 12:25:55 +0600

    cryptography codes

--- 2.cpp
+++ 2.cpp
@@ -8,15 +8,61 @@ using namespace std;
 
 string PlainTextToCipther(string text)
 {
-    for(int i = 0; i < text.size(); i++)
+    for(int i = 0; i < text.size(); i = i+3)
     {
-        for(int j=i; j < i+2 ; j++)
+        for(int j = i; j < i+3 ; j++)
         {
+            /**
+             * if there is single character left
+            */
+            if(j+1 >= text.size())
+            {
+                if ((text[j] >= 'A' && text[j] <= 'Z') || (text[j] >= 'a' && text[j] <= 'z'))
+                {
 
-        }
+                    if (text[j] >= 'A' && text[j] <= 'Z')
+                    {
 
-        i = i+2;
-    }
+                        int code = (int)text[j] + 5;
+
+                        code = code - (int)'A';
+
+                        if ((code - 26) >= 0)
+                        {
+                            code = code - 26;
+                        }
+
+                        code = code + (int)'A';
+
+                        text[j] = (char)code;
+                    }
+
+                    if (text[j] >= 'a' && text[j] <= 'z')
+                    {
+                        int code = (int)text[j] + 5;
+
+                        code = code - (int)'a';
+
+                        if ((code - 26) >= 0)
+                        {
+                            code = code - 26;
+                        }
+
+                        code = code + (int)'a';
+
+                        text[j] = (char)code;
+                    }
+                }
+            }
+
+            /**
+             * if there is two character left
+            */
+           
+            if()
+
+        }// j loop
+    }// i loop
 }
 
 string CiptherToPlainText(string encrypt)
