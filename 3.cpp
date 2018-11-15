a42043d59d59b6afe34fffd4676d779fb4158d4b
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-15 12:31:28 +0600

    encryption

--- 3.cpp
+++ 3.cpp
@@ -2,9 +2,15 @@
 #include <string>
 using namespace std;
 
+string TextToCipher(string text)
+{
+    
+}
 
 main(int argc, char const *argv[])
 {
-    string text = ""
+    string text = "DEPARTMENT OF COMPUTER SCIENCE AND TECHNOLOGY UNIVERSITY OF RAJSHAHI BANGLADESH";
+
+
     return 0;
 }
