bde7740fb5fed318f5e8f6ef8df236d58cac2371
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-11-15 13:15:21 +0600

    encryption

--- 3.cpp
+++ 3.cpp
@@ -26,10 +26,38 @@ string TextToCipher(string text)
                 text[k] = temp;
             }
         }
-
     }
 
     return text;
+}
+
+string CipherToText(string encrypt)
+{
+    for (int i = 0; i < encrypt.size(); i++)
+    {
+        int previous_position = i;
+
+        while (encrypt[i] != ' ' && encrypt[i] != '\0')
+        {
+            i++;
+        }
+
+        int space_position = i;
+
+        //cout << previous_position << " " << space_position << endl;
+
+        for (int j = previous_position; j < space_position; j++)
+        {
+            for (int k = space_position - 1; k >= previous_position; k--)
+            {
+                char temp = encrypt[j];
+                encrypt[j] = encrypt[k];
+                encrypt[k] = temp;
+            }
+        }
+    }
+
+    return encrypt;
 }
 
 main(int argc, char const *argv[])
@@ -43,6 +71,10 @@ main(int argc, char const *argv[])
     string encrypt = TextToCipher(text);
 
     cout<<"Encrypted text: "<< encrypt << endl;
+
+    string dercypt = CipherToText(encrypt);
+
+    cout<< "Decypted text: "<< dercypt <<endl;
 
     return 0;
 }
