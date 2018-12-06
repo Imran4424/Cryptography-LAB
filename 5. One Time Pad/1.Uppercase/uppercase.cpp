35e0845a2810149c135807f47678961d05a48954
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2018-12-05 22:20:08 -0800

    one time pad

--- 5. One Time Pad/1.Uppercase/uppercase.cpp
+++ 5. One Time Pad/1.Uppercase/uppercase.cpp
@@ -8,6 +8,60 @@
 #include <fstream>
 using namespace std;
 
+string PlaintextToCipher(string text)
+{
+    
+
+
+    for(int i = 0; i < text.size(); i++)
+    {
+        
+        if ((text [i] >= 'A' && text[i] <= 'Z') || (text[i] >= 'a' && text[i] <= 'z')) 
+        {
+
+            if (text [i] >= 'A' && text[i] <= 'Z') 
+            {
+
+                int code = ((int) text[i] + 3);
+
+                code = code - (int) 'A';
+
+                
+                if ((code - 26) >= 0) 
+                {
+                    code = code - 26;    
+                }
+                
+                code = code + (int) 'A';
+
+                text[i] = (char) code;
+            }
+
+            
+            if (text[i] >= 'a' && text[i] <= 'z') 
+            {
+                int code = (int) text[i] + 3;
+
+                code = code - (int) 'a';
+
+                
+                if ((code - 26) >= 0) 
+                {
+                    code = code - 26;    
+                }
+                
+                code = code + (int) 'a';
+
+                text[i] = (char) code;
+            }
+            
+        }
+        
+    }
+
+    return text;
+}
+
 
 int main(int argc, char const *argv[])
 {
