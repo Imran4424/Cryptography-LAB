ec1f25707b71f3970616441ebc7c379469aac6f7
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-10 12:58:15 +0600

    lehman

--- 6. Lehmann Algorithm/Lehmann.cpp
+++ 6. Lehmann Algorithm/Lehmann.cpp
@@ -3,22 +3,49 @@
 #include <fstream>
 using namespace std;
 
+typedef unsigned long long int ulli;
+
+ulli my_pow(ulli base, ulli expo, ulli mod)
+{
+	ulli result = base;
+
+	for(ulli i = 1; i <= expo; i++)
+	{
+		result = (result * base) % mod;
+	}
+
+	return result;
+}
 
 int main(int argc, char const *argv[])
 {
-	int num;
+	ulli num;
 	cout << "enter the number which you want to be tasted" << endl;
 	cin >> num;
 
 
-	int times;
+	ulli times;
 	cout << "How many times you want to repeat the number" << endl;
 	cin >> times;
 
 
-	for(int t = 1; t <= times; t++)
+	for(ulli t = 1; t <= times; t++)
 	{
-		int randomNum = rand()
+		ulli randomNum = rand() % (num-1) + 1;
+
+		ulli expo = (num-1) / 2 ;
+
+		ulli result = my_pow(randomNum, expo, num);
+
+		
+		
+		if ( result % num != 1 && result % num != (num - 1))
+		{
+			cout << num << " is definitely not prime " << endl;
+
+			break;
+		}
+	
 	}
 
 
