c95572b1fa960961805310646279d8230941db50
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-01-10 08:20:33 +0600

    lehman

--- 6. Lehmann Algorithm/Lehmann.cpp
+++ 6. Lehmann Algorithm/Lehmann.cpp
@@ -6,9 +6,25 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+	int num;
+	cout << "enter the number which you want to be tasted" << endl;
+	cin >> num;
 
-    return 0;
+
+	int times;
+	cout << "How many times you want to repeat the number" << endl;
+	cin >> times;
+
+
+	for(int t = 1; t <= times; t++)
+	{
+		int randomNum = rand()
+	}
+
+
+	return 0;
 }
 
-
-
+/*
+	GMP - c++ library for big numbers
+*/
