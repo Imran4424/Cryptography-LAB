8203b1c2305e13062662b8a0fc42d7bd33357c5f
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-02-07 02:05:37 +0600

    lehmann

--- 7. Robin-Miller/RobinMiller.cpp
+++ 7. Robin-Miller/RobinMiller.cpp
@@ -8,6 +8,11 @@ typedef unsigned long long int ulli;
 
 int main(int argc, char const *argv[])
 {
+	bool status = true;
+
+	ulli num;
+	cout << "enter the number which you want to be tasted" << endl;
+	cin >> num;
 	
 	return 0;
 }
