2249d49da068d0f670050e0b852355c2c0c03739
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-02-06 23:01:36 +0600

    lehmann

--- 6. Lehmann Algorithm/Lehmann.java
+++ 6. Lehmann Algorithm/Lehmann.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 class Lehman
 {
+	int my_pow(int base, int expo, int mod)
+	{
+		int result = base;
+
+		for(int i = 1; i <= expo; i++)
+		{
+			result = (result * base) % mod;
+		}
+
+		return result;
+	}
+
+
 	public static void main(String[] args) 
 	{
+		Scanner cin = new Scanner(System.in);
+
 		
 	}
 }
