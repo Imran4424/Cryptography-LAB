9b9903293121bb3516ef5ed8799c5ac1571e2cbf
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-02-07 08:56:12 +0600

    robin miller

--- 10. RSA/RSA.cpp
+++ 10. RSA/RSA.cpp
@@ -2,9 +2,53 @@
 #include <string>
 using namespace std;
 
+typedef long long int lli;
+
+int gcd(int a, int b) 
+{ 
+    int temp; 
+
+    while (1) 
+    { 
+        temp = a % b; 
+        if (temp == 0)
+        {	
+          return b; 
+        } 
+        a = b; 
+        b = temp; 
+    } 
+} 
+
 
 int main(int argc, char const *argv[])
 {
-	
+	lli p = 53, q = 59;
+
+	lli n = p*q;
+
+	lli phiN = (p-1)*(q-1);
+
+	// encryption key;
+
+	lli eKey = 2;
+
+	while(eKey < phiN)
+	{
+		if( gcd(eKey, phiN) == 1)
+		{
+			break;
+		}
+		else
+		{
+			eKey++;
+		}
+	}
+
+	// decryption key
+
+	int k = 2;
+	lli dKey = (1 + (k*phiN)) / eKey;
+
 	return 0;
 }
