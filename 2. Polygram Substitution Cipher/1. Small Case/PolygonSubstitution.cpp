f2d1b5799c64f48b572d47e66010a2184e6687a5
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-02-07 10:35:55 +0600

    cryptography

--- 2. Polygram Substitution Cipher/1. Small Case/PolygonSubstitution.cpp
+++ 2. Polygram Substitution Cipher/1. Small Case/PolygonSubstitution.cpp
@@ -2,10 +2,13 @@
 #include <string>
 #include <map>
 #include <fstream>
+#include <algorithm>
 using namespace std;
 
 string PlaintextToCipher(string text)
 {
+	replace(text.begin(), text.end(), ' ', '_');
+
 	map <string, string> encryptDictionary;
 
 	ifstream readFile;
@@ -37,9 +40,78 @@ string PlaintextToCipher(string text)
 
 		if(block.length() == 3)
 		{
-			
+			block = encryptDictionary[block];
+
+			int x,y;
+
+			for(x = startPos, y = 0; y < 3 ; y++, x++)
+			{
+				text[x] = block[y];
+			}
+
+			startPos = x;
+
+			block.clear();
 		}
 	}
+
+	return text;
+}
+
+string CipherToPlaintext(string encrypt)
+{
+
+
+	map <string, string> decryptDictionary;
+
+	ifstream readFile;
+
+	string msg, cipher;
+
+	readFile.open("dictionary.txt");
+
+	if (readFile.is_open())
+	{
+		while(readFile >> msg >> cipher)
+		{
+			decryptDictionary[cipher] = msg;
+		}
+
+		readFile.close();
+	}
+	else
+	{
+		cout << "can not open the file" << endl;
+	}
+
+	string block;
+	int startPos = 0;
+
+
+	for(int i = 0; i < encrypt.length(); i++)
+	{
+		block += encrypt[i];
+
+		if(block.length() == 3)
+		{
+			block = decryptDictionary[block];
+
+			int x,y;
+
+			for(x = startPos, y = 0; y < 3 ; y++, x++)
+			{
+				encrypt[x] = block[y];
+			}
+
+			startPos = x;
+
+			block.clear();
+		}
+	}
+
+	replace(encrypt.begin(), encrypt.end(), '_', ' ');
+	
+	return encrypt;
 }
 
 
@@ -47,7 +119,9 @@ int main(int argc, char const *argv[])
 {
 	string text;
 
-	getline(cin, text);
+	//getline(cin, text);
+
+	text = "i love to play games";
 
 	cout << "......................" <<endl;
 
