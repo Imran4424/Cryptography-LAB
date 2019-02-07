4bd8b95f6511694ecdd36628ef69af9d6f2cdc97
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2019-02-07 06:20:09 +0600

    robin miller

--- 7. Robin-Miller/RobinMiller.cpp
+++ 7. Robin-Miller/RobinMiller.cpp
@@ -5,6 +5,30 @@ using namespace std;
 
 typedef unsigned long long int ulli;
 
+ulli MyPow(ulli base, ulli pow)
+{
+	result = 1;
+
+	for (int i = 1; i <= pow; ++i)
+	{
+		result *= base;
+	}
+
+	return result;
+}
+
+ulli MyPowEx(ulli base, ulli expo, ulli mod)
+{
+	ulli result = base;
+
+	for(ulli i = 1; i < expo; i++)
+	{
+		result = (result * base) % mod;
+	}
+
+	return result;
+}
+
 
 int main(int argc, char const *argv[])
 {
@@ -14,6 +38,34 @@ int main(int argc, char const *argv[])
 	cout << "enter the number which you want to be tasted" << endl;
 	cin >> num;
 
-	
+
+	// finding b and m
+
+	ulli b = 1, m;
+
+	ulli dividor = MyPow(2, b);
+
+	while((num - 1) % dividor == 0)
+	{
+		b++;
+
+		dividor = MyPow(2,b);
+	}
+
+	b--;
+
+	dividor = MyPow(2,b);
+
+	m = (num - 1) / dividor;
+
+
+	// chosing a 
+
+	ulli randomA = rand() (num-1) + 1;
+
+
+	 = MyPowEx(randomA, m, num);
+
+
 	return 0;
 }
